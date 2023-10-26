41f937d458765fa1676d15cd38772371422ecc9a
badiskefi <badiskefi@gmail.com>
2023-10-26 12:29:12 +0200

    adding banner top 2

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -60,18 +60,18 @@ export default function RootLayout({
       </div>
       <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
         <p className="text-sm leading-6 text-gray-900">
-          <strong className="font-semibold">GeneriCon 2023</strong>
+          <strong className="font-semibold">Project in Progress</strong>
           <svg viewBox="0 0 2 2" className="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true">
             <circle cx={1} cy={1} r={1} />
           </svg>
-          Join us in Denver from June 7 – 9 to see what’s coming next.
+          currently working on this project so you may face some limmitation or some features not working, will be done by 16/11/2023.
         </p>
-        <a
+        {/* <a
           href="#"
           className="flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
         >
           Register now <span aria-hidden="true">&rarr;</span>
-        </a>
+        </a> */}
       </div>
       <div className="flex flex-1 justify-end">
         <button type="button" className="-m-3 p-3 focus-visible:outline-offset-[-4px]">
